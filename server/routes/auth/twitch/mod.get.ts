1c2b2fb56adfc23f13b9c8888e73068ee9b2f34f
Ahmed Rangel <ahmedrangel@outlook.com>
2026-01-11 18:38:59 -0500

    Update mod.get.ts

--- server/routes/auth/twitch/mod.get.ts
+++ server/routes/auth/twitch/mod.get.ts
@@ -12,6 +12,7 @@ export default defineOAuthTwitchEventHandler({
     const twitchApi = new ApiClient({ authProvider: provider });
     const moderatedChannelsPagination = twitchApi.moderation.getModeratedChannelsPaginated(twitch.id);
     const moderatedChannels = await moderatedChannelsPagination.getAll();
+    console.info(moderatedChannels.map(c => ({ id: c.id, name: c.name })));
     if (moderatedChannels.map(c => c.id).includes(SITE.twitchId)) {
       await db.insert(tables.channelModerators).values({
         twitchId: twitch.id,
