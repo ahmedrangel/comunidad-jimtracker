8009275f999113f712fda7fac5825d73b9183f1f
Ahmed Rangel <ahmedrangel@outlook.com>
2026-01-11 16:27:50 -0500

    feat: restrict twitch mod auth to jimrsng moderators only

--- server/routes/auth/twitch/mod.get.ts
+++ server/routes/auth/twitch/mod.get.ts
@@ -1,25 +1,39 @@
+import { StaticAuthProvider } from "@twurple/auth";
+import { ApiClient } from "@twurple/api";
+
 export default defineOAuthTwitchEventHandler({
   config: {
-    scope: ["moderator:read:followers"]
+    scope: ["user:read:moderated_channels", "moderator:read:followers"]
   },
   async onSuccess (event, result: { user: TwitchUser, tokens: any }) {
     const twitch = result.user;
-
-    await db.insert(tables.channelModerators).values({
-      twitchId: twitch.id,
-      accessToken: result.tokens.access_token,
-      refreshToken: result.tokens.refresh_token,
-      expiresIn: result.tokens.expires_in
-    }).onConflictDoUpdate({
-      target: tables.channelModerators.twitchId,
-      set: {
+    const config = useRuntimeConfig(event);
+    const provider = new StaticAuthProvider(config.oauth.twitch.clientId, result.tokens.access_token);
+    const twitchApi = new ApiClient({ authProvider: provider });
+    const moderatedChannelsPagination = twitchApi.moderation.getModeratedChannelsPaginated(twitch.id);
+    const moderatedChannels = await moderatedChannelsPagination.getAll();
+    if (SITE.twitchId in moderatedChannels.map(c => c.id)) {
+      await db.insert(tables.channelModerators).values({
+        twitchId: twitch.id,
         accessToken: result.tokens.access_token,
         refreshToken: result.tokens.refresh_token,
-        expiresIn: result.tokens.expiresIn,
-        updatedAt: unixepoch({ mode: "ms" })
-      }
-    }).returning().get();
-
-    return sendRedirect(event, "/");
+        expiresIn: result.tokens.expires_in
+      }).onConflictDoUpdate({
+        target: tables.channelModerators.twitchId,
+        set: {
+          accessToken: result.tokens.access_token,
+          refreshToken: result.tokens.refresh_token,
+          expiresIn: result.tokens.expiresIn,
+          updatedAt: unixepoch({ mode: "ms" })
+        }
+      }).returning().get();
+      return sendRedirect(event, "/");
+    }
+    else {
+      throw createError({
+        status: ErrorCode.FORBIDDEN,
+        message: "No eres moderador de JimRsng."
+      });
+    }
   }
 });
