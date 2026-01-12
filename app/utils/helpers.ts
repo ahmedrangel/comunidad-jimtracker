6da5a49d031677eeca7383771cfb08efda42c65c
Ahmed Rangel <ahmedrangel@outlook.com>
2026-01-12 01:23:19 -0500

    chore: remove icon verification 29

--- app/utils/helpers.ts
+++ app/utils/helpers.ts
@@ -3,7 +3,7 @@ export const getIconURL = (iconId: number) => {
 };
 
 export const getRandomIconId = () => {
-  const creationIcons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29];
+  const creationIcons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28];
   const randomIndex = Math.floor(Math.random() * creationIcons.length);
   return creationIcons[randomIndex]!;
 };
