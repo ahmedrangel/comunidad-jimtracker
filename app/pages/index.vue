cda9e0aad61fcbaa0bf576aec96cb5c0d9dea0e2
Ahmed Rangel <ahmedrangel@outlook.com>
2026-01-14 00:19:05 -0500

    chore: adjust wr bar max-w

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -287,7 +287,7 @@ onMounted(() => {
           <template #wins-losses-cell="{ row }">
             <div class="flex flex-col items-center justify-center gap-1 min-w-24">
               <span class="font-semibold"><span class="text-blue-400">{{ row.original.wins }}</span> V <span class="text-dimmed">|</span> <span class="text-rose-400">{{ row.original.losses }}</span> D</span>
-              <UProgress v-model="row.original.wins" :max="row.original.wins + row.original.losses" size="lg" class="max-w-30 w-full" :ui="{ base: 'bg-rose-400', indicator: 'bg-blue-400 rounded-none' }" />
+              <UProgress v-model="row.original.wins" :max="row.original.wins + row.original.losses" size="lg" class="max-w-26 w-full" :ui="{ base: 'bg-rose-400', indicator: 'bg-blue-400 rounded-none' }" />
             </div>
           </template>
           <template #matches-cell="{ row }">
