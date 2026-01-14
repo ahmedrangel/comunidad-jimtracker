ca988b83dcca3e56b1a4dc98fb2a0a721eb2e669
Ahmed Rangel <ahmedrangel@outlook.com>
2026-01-14 16:40:27 -0500

    fix: add user session validation to verify-follow endpoint

--- server/api/users/[name]/verify-follow.ts
+++ server/api/users/[name]/verify-follow.ts
@@ -2,16 +2,20 @@ import { StaticAuthProvider, refreshUserToken } from "@twurple/auth";
 import { ApiClient } from "@twurple/api";
 
 export default defineEventHandler(async (event) => {
+  const { user } = await requireUserSession(event);
+  const { twitchId } = user;
+
   const params = await getValidatedRouterParams(event, z.object({
     name: z.string()
   }).parse);
 
+  if (params.name !== user.twitchLogin) {
+    throw createError({ status: ErrorCode.FORBIDDEN, message: "No tienes permiso para realizar esta acción" });
+  }
+
   const config = useRuntimeConfig(event);
 
-  const [twitchId, userModerator] = await Promise.all([
-    getTwitchIdByLogin(event, params.name),
-    db.select().from(tables.channelModerators).all()
-  ]);
+  const userModerator = await db.select().from(tables.channelModerators).all();
 
   if (!userModerator.length) {
     throw createError({ status: ErrorCode.NOT_FOUND, message: "Ha ocurrido un error." });
