509e3e2052da0bf4e964d35ce480118e7001b744
Yizack Rangel <yizackr@gmail.com>
2026-01-14 23:39:56 -0500

    feat: add popover handlers to tier image in elo cell

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -337,7 +337,11 @@ onMounted(() => {
           </template>
           <template #elo-cell="{ row }">
             <div class="flex items-center justify-center gap-1">
-              <img :src="`/images/lol/${row.original.tier?.toLowerCase() || 'unranked'}.png`" class="w-10 h-10 md:w-10 md:h-10 max-w-fit" :title="row.original.tier">
+              <img
+                :src="`/images/lol/${row.original.tier?.toLowerCase() || 'unranked'}.png`"
+                class="w-10 h-10 md:w-10 md:h-10 max-w-fit"
+                v-on="popoverHandlers(row.original.tier || 'UNRANKED')"
+              >
               <span v-if="row.original.division || row.original.lp"><span v-if="!['MASTER', 'GRANDMASTER', 'CHALLENGER'].includes(row.original.tier)">{{ row.original.division }} · </span>{{ row.original.lp }} LP</span>
             </div>
           </template>
