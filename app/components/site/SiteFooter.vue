79b3afdb919dc1ea4a9bed8cdaa36b97f0d3fcad
Ahmed Rangel <ahmedrangel@outlook.com>
2026-02-13 14:36:31 -0500

    chore(footer): add disclaimer

--- app/components/site/SiteFooter.vue
+++ app/components/site/SiteFooter.vue
@@ -10,24 +10,30 @@ const timeAgo = useTimeAgoIntl(buildInfo.time, {
 <template>
   <UFooter class="border-t border-default">
     <template #left>
-      <div class="flex flex-col lg:flex-row items-center gap-2">
-        <p class="text-muted text-sm">{{ SITE.name }} © {{ new Date().getFullYear() }}</p>
-        <UPopover mode="hover" :content="{ side: 'top' }" arrow>
-          <NuxtLink :to="`${SITE.github.repository}/commit/${buildInfo.commit}`" target="_blank">
-            <UBadge
-              color="neutral"
-              variant="subtle"
-              class="flex hover:text-highlighted hover:underline text-muted"
-            >
-              <span class="font-mono">Build {{ buildInfo.shortCommit }}</span>
-            </UBadge>
-          </NuxtLink>
-          <template #content>
-            <p>{{ timeAgo }}</p>
-          </template>
-        </UPopover>
+      <div class="flex flex-col gap-2 lg:gap-1 lg:text-left text-center">
+        <div class="flex flex-col lg:flex-row items-center gap-2">
+          <p class="text-sm">{{ SITE.name }} © {{ new Date().getFullYear() }}</p>
+          <UPopover mode="hover" :content="{ side: 'top' }" arrow>
+            <NuxtLink :to="`${SITE.github.repository}/commit/${buildInfo.commit}`" target="_blank">
+              <UBadge
+                color="neutral"
+                variant="subtle"
+                class="flex hover:text-highlighted hover:underline text-muted"
+              >
+                <span class="font-mono">Build {{ buildInfo.shortCommit }}</span>
+              </UBadge>
+            </NuxtLink>
+            <template #content>
+              <p>{{ timeAgo }}</p>
+            </template>
+          </UPopover>
+        </div>
+        <p class="text-muted text-sm max-w-150 lg:max-w-full">
+          JimTracker no está respaldado por Riot Games y no refleja las opiniones o puntos de vista de Riot Games ni de nadie involucrado oficialmente en la producción o gestión de League of Legends.
+        </p>
       </div>
     </template>
+    <template #center />
     <span class="text-sm">
       <NuxtLink :to="SITE.github.repository" target="_blank" class="dark:text-blue-300 light:text-blue-500 font-semibold hover:underline">Creado</NuxtLink>
       con ❤️ por
