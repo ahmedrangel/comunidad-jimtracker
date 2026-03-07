99343f639ebf57dc52a3dfe2b19c4c3a33e26ed0
Yizack Rangel <yizackr@gmail.com>
2026-03-07 15:16:33 -0500

    chore: remove deprecated error props

--- app/error.vue
+++ app/error.vue
@@ -9,8 +9,8 @@ defineProps<{
 <template>
   <div class="flex items-center justify-center h-screen bg-default">
     <div class="rounded-md bg-elevated p-10 shadow-lg border border-accented">
-      <h1 class="text-4xl font-bold text-primary mb-2">Error {{ error.statusCode }}</h1>
-      <h5>Ha ocurrido un error: {{ error.message || error.statusMessage || "error" }}</h5>
+      <h1 class="text-4xl font-bold text-primary mb-2">Error {{ error.status }}</h1>
+      <h5>Ha ocurrido un error: {{ error.message || error.statusText || "error" }}</h5>
       <span>Volver: <ULink to="/" class="underline">{{ SITE.domain }}</ULink></span>
     </div>
   </div>
