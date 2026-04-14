850b1ae7161e594775d9610d82ef6613b4d22c73
Ahmed Rangel <ahmedrangel@outlook.com>
2026-04-13 21:39:54 -0500

    chore: update top tier rank gold badge to top 20

--- app/components/RankBadge.vue
+++ app/components/RankBadge.vue
@@ -3,12 +3,12 @@ const props = defineProps<{
   rank: number;
 }>();
 
-const isTop10 = computed(() => props.rank > 0 && props.rank <= 10);
+const isTop = computed(() => props.rank > 0 && props.rank <= 20);
 </script>
 
 <template>
-  <div class="rank-badge relative before:absolute rounded overflow-hidden border flex items-center justify-start" :class="[isTop10 ? 'dark:bg-yellow-400 dark:before:bg-yellow-950 bg-yellow-700 before:bg-yellow-100 border-yellow-700 dark:border-yellow-400' : 'dark:bg-white dark:before:bg-black/80 bg-black/70 before:bg-white border-black/70 dark:border-white']" :title="`Rank #${rank}`">
-    <span class="relative text-lg font-semibold ps-2 pe-4" :class="[isTop10 ? 'dark:text-yellow-400 text-yellow-700' : 'text-normal']">#{{ rank }}</span>
+  <div class="rank-badge relative before:absolute rounded overflow-hidden border flex items-center justify-start" :class="[isTop ? 'dark:bg-yellow-400 dark:before:bg-yellow-950 bg-yellow-700 before:bg-yellow-100 border-yellow-700 dark:border-yellow-400' : 'dark:bg-white dark:before:bg-black/80 bg-black/70 before:bg-white border-black/70 dark:border-white']" :title="`Rank #${rank}`">
+    <span class="relative text-lg font-semibold ps-2 pe-4" :class="[isTop ? 'dark:text-yellow-400 text-yellow-700' : 'text-normal']">#{{ rank }}</span>
   </div>
 </template>
 
